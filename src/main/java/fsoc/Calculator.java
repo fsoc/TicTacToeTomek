fa661c11372ac694f24f13f57434c9cba073697b
fsoc <fsoc@altai.se>
2016-05-28 02:22:53 +0200

    Cleaned up code and added better tests.

--- src/main/java/fsoc/Calculator.java
+++ src/main/java/fsoc/Calculator.java
@@ -3,14 +3,14 @@ package fsoc;
 /**
  *
  */
-public class Calculator {
+class Calculator {
   public static String process(BoardCounter bc, int maxRows) {
-    char[] chars = {'O', 'X'};
 
     for (int i = 0; i < maxRows; i++) {
       for (Sign sign : Sign.values()) {
         if (bc.getRows(sign)[i] == maxRows || bc.getCols(sign)[i] == maxRows || (
-                i < bc.getDiags(sign).length && bc.getDiags(sign)[i] == maxRows )) {
+                i < bc.getDiags(sign).length &&
+                        bc.getDiags(sign)[i] == maxRows )) {
           return sign.name() + " won";
         }
       }
--- src/test/java/fsoc/CalculatorTest.java
+++ src/test/java/fsoc/CalculatorTest.java
@@ -1,16 +1,44 @@
 package fsoc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
  */
 public class CalculatorTest {
-  private Calculator c;
+  private BoardCounter bc;
+
+  @Before
+  public void initialize() {
+    bc = mock(BoardCounter.class);
+  }
 
   @Test
-  public void fullXRowTest() {
+  public void XWonTest() {
+    when(bc.getRows(Sign.X)).thenReturn(new int[]{3, 2, 1});
+    assertEquals("X won", Calculator.process(bc, 3));
+  }
+
+  @Test
+  public void DiagWonTest() {
+    when(bc.getRows(Sign.X)).thenReturn(new int[]{1, 2, 1});
+    when(bc.getCols(Sign.X)).thenReturn(new int[]{1, 2, 1});
+    when(bc.getDiags(Sign.X)).thenReturn(new int[]{3, 0});
+    assertEquals("X won", Calculator.process(bc, 3));
+  }
+
+  @Test
+  public void DrawTest() {
+    when(bc.getRows(any())).thenReturn(new int[]{0, 0, 0});
+    when(bc.getCols(any())).thenReturn(new int[]{0, 0, 0});
+    when(bc.getDiags(any())).thenReturn(new int[]{0, 0});
+    when(bc.hasUnsetElements()).thenReturn(false);
+    assertEquals("Draw", Calculator.process(bc, 3));
   }
 }
